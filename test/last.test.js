f1a90b4f5facbda8bd8cfdee815995086ebeb6bb
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-09 16:40:26 -0700

    Add test to last()

--- test/last.test.js
+++ test/last.test.js
@@ -12,4 +12,8 @@ describe('last()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns undefined if you ask for the last element of an empty array', () => {
+    expect(_.last([])).toEqual(undefined);
+  });
 });
