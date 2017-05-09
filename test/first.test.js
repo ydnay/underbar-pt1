dab7a3a1c0f82811e2c48b795910cba5c5871627
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-09 16:15:39 -0700

    Add test to first()

--- test/first.test.js
+++ test/first.test.js
@@ -12,4 +12,8 @@ describe('first()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.first(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns an empty array if we pass an empty array', () => {
+    expect(_.first([], 2)).toEqual([]);
+  });
 });
