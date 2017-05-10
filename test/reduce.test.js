261799f59024b2ece31092bcb61eabecab5377f8
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-09 17:37:10 -0700

    Add test to reduce()

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -13,4 +13,15 @@ describe('reduce()', () => {
     expect(result).toEqual(15);
   });
 
+  it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
+    const nums = {
+      blowin: 1,
+      in: 2,
+      the: 3,
+      wind: 4,
+    };
+    const result = _.reduce(nums, (a, b) => a + b);
+    expect(result).toEqual(10);
+  });
+
 });
