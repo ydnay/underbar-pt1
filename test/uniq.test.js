3fa618a906f1f9ab6036ab4a2980689daec7526d
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-09 17:54:03 -0700

    Add test to uniq()

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,13 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups a list of songs', () => {
+    const songs = {
+      heyJude: 1,
+      anotherSong: 2,
+      andAnotherSong: 1,
+    };
+    expect(_.uniq(songs)).toEqual([1, 2]);
+  });
 });
