70e3a37c1011fdcce0c4a3c88cbea40c69ce1c6a
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-05-09 17:42:15 -0700

    Add test to reject()

--- test/reject.test.js
+++ test/reject.test.js
@@ -12,10 +12,24 @@ describe('reject()', () => {
       side: null,
       condiment: 'ketchup',
       drink: null,
-      dessert: 'cookie'
+      dessert: 'cookie',
     };
 
     const orderItems = _.reject(order, (value) => value === null);
     expect(orderItems).toEqual(['burger', 'ketchup', 'cookie']);
+  });
+
+  it('rejects non-numeric values from an object', () => {
+    const order = {
+      entree: 'burger',
+      side: 1,
+      condiment: 'ketchup',
+      drink: 2,
+      dessert: 'cookie',
+      check: NaN,
+    };
+
+    const orderItems = _.reject(order, (value) => typeof (value) !== 'number');
+    expect(orderItems).toEqual([1, 2, NaN]);
   });
 });
